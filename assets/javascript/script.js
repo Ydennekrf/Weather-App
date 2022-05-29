7d1c1056f05daac8acfcd8eae71531a85ea95090
ydennekrf <ydennekrf@gmail.com>
2022-05-29 09:36:21 -0400

    set up html page for test printing

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,40 +3,46 @@ let cityLat = 43.6534817;
 let cityLong = -79.3839347;
 let lang = "en";
 let apiKey = "a772a40f5da703f3736db6d33655ff2f"
+testInfo = document.querySelector("#test-info");
 
 // fetches the Geo API info to get city coordinates
-geoApi = () => {
-    let getGeo = `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=5&appid=${apiKey}`
-    console.log(cityInput)
-    fetch(getGeo)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (geoData) {
-            console.log(geoData)
-            cityLong = geoData[0].lon
-            cityLat = geoData[0].lat
-            getWeather();           
-        })
+// geoApi = () => {
+//     let getGeo = `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=5&appid=${apiKey}`
+//     console.log(cityInput)
+//     fetch(getGeo)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (geoData) {
+//             
+//             cityLong = geoData[0].lon
+//             cityLat = geoData[0].lat
+//             getWeather();           
+//         })
 
-};
+// };
 
-getWeather = () => {
-    let selectedCity = `api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLong}&units=metric&lang=${lang}&exclude=alerts,minutely&appid=${apiKey}`
+// getWeather = () => {
+//     let selectedCity = `http://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLong}&units=metric&lang=${lang}&exclude=alerts,minutely&appid=${apiKey}`
 
-    fetch(selectedCity)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (cityData) {
-            console.log(cityData)
-            localStorage.setItem("cityName", JSON.stringify(cityData))
-        })
+//     fetch(selectedCity)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (cityData) {
+//             console.log(cityData)
+//             localStorage.setItem("cityName", JSON.stringify(cityData))
+                // loadWeather();
+//         })
+// }
+
+loadWeather = () => {
+    let weatherData = JSON.parse(localStorage.getItem(cityName))
+
 }
 
-
 // geoApi();
-getWeather();
+// getWeather();
 
 //adds event listener to the search button
 // searchBtn.addEventListener("click", geoApi);
